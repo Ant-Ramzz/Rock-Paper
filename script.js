93e5c645025d297033f08fcd26b16c3e71ec08d6
Tony R <anthony.e.tovar@gmail.com>
2023-12-03 11:36:12 -0600

    Completed program Basic

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function getComputerChoice(){
 }
 function getPlayerChoice(){
     
-    let pChoice = prompt("Choose Rock, Paper or Scissors!").toUpperCase
-    while (choice.includes(pChoice) = False) {
-        let pChoice = prompt("Invalid option!  Please choose Rock, Paper or Scissors!").toUpperCase
+    let pChoice = window.prompt("Choose Rock, Paper or Scissors!").toLowerCase();
+    while (choice.includes(pChoice) == false) {
+         pChoice = window.prompt("Invalid option!  Please choose Rock, Paper or Scissors!").toLowerCase();
     }
     return(pChoice)
 }
@@ -16,18 +16,27 @@ function playround(pSelection, cSelection){
         console.log("Tie, try again");
         return playround(getPlayerChoice(),getComputerChoice());
     }
-    return (pSelection == "ROCK" &&  cSelection == "PAPER"||pSelection == "PAPER" &&  cSelection == "ROCK" 
-    || pSelection == "SCISSORS" &&  cSelection == "PAPER" ) ?`You Win! ${pSelection} beats ${cSelection}.` :
-    `You Lose! ${pSelection} beats ${cSelection}.` 
+    return (pSelection == "rock" &&  cSelection == "scissors"||pSelection == "paper" &&  cSelection == "rock" 
+    || pSelection == "scissors" &&  cSelection == "paper" ) ?`You Win! ${pSelection} beats ${cSelection}.` :
+    `You Lose! ${cSelection} beats ${pSelection}.` ;
     
 }
 
 
 
 function game(){
-    
+    console.log("Best of 5 Rock Paper Scissors!");
+    let wons = 0;
+    let t
+    for (let i =1;i<=5;i++){
+        
+        t =playround(getPlayerChoice(),getComputerChoice());
+        t.includes("Win") ? wons++:0;
+        console.log(t);
+    }
+    (wons <3) ? console.log("You los this set"):console.log("You won this set")
 }
 
-const choice =["ROCK", "PAPER", "SCISSORS"]
-
+const choice =["rock", "paper", "scissors"]
+game()
 
